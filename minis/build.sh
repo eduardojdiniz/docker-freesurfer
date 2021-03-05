1c142ec6ac7409b45af74950f0f5528891feda84
eduardojdiniz <edd32@pitt.edu>
2021-03-05 18:57:52 -0500

    Remove minis folder

--- minis/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-docker build -t brainlife/freesurfer-mini . 
-docker tag brainlife/freesurfer-mini brainlife/freesurfer-mini:7.1.1
-docker push brainlife/freesurfer-mini
--- minis/test.sh
+++
@@ -1,2 +0,0 @@
-echo $FREESURFER_LICENSE > license
-singularity exec -B `pwd`/license:/usr/local/freesurfer/.license docker://brainlife/freesurfer-mini:7.1.1 mri_convert ../test.mgz test.nii.gz
